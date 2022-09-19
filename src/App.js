b41795c298a2f379db21fbb0b399a01d4d56dd5d
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-09-19 16:28:35 -0400

    Created basic income display structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import ExpenseDisplay from "./components/ExpenseDisplay/ExpenseDisplay";
 import Header from "./components/Header/Header";
+import IncomeDisplay from "./components/IncomeDisplay/IncomeDisplay";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ExpenseDisplay />
+      {/* <ExpenseDisplay /> */}
+      <IncomeDisplay />
     </div>
   );
 }
--- src/components/IncomeDisplay/IncomeDisplay.js
+++ src/components/IncomeDisplay/IncomeDisplay.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./IncomeDisplay.scss";
 
 export default function IncomeDisplay() {
-  return <div>IncomeDisplay</div>;
+  return (
+    <div className="income">
+      <h2>Income</h2>
+      <div className="income__details">
+        <h3 className="income__month">Month</h3>
+        <h3 className="income__amount">Amount</h3>
+      </div>
+    </div>
+  );
 }
