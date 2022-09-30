89b7be959caf288ff99065db90f437eb06a7bb56
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-09-30 18:13:02 -0400

    Edited navigator component to have an income block to input and display income amount

--- src/components/Navigator/Navigator.js
+++ src/components/Navigator/Navigator.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./Navigator.scss";
 
 export default function Navigator() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => setInput(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="tabs">
-      <h2 className="tabs__tab">Income</h2>
+      <div className="tabs__tab">
+        <h2>Income</h2>
+        <form>
+          <input name="income" value={input} onChange={handleChange}></input>
+          <button>Add</button>
+        </form>
+      </div>
       <h2 className="tabs__tab">Expenses</h2>
       <h2 className="tabs__tab">Savings</h2>
     </div>
