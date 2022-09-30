c46c4ee134edfe306958298b4ecfa81f86bb5ab9
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-09-30 18:27:08 -0400

    Updated income state logic to display income state on submit

--- src/components/Navigator/Navigator.js
+++ src/components/Navigator/Navigator.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addIncome } from "../../features/income/incomeSlice";
 import "./Navigator.scss";
 
 export default function Navigator() {
   const [input, setInput] = useState("");
+  const { income } = useSelector((store) => store.income);
   const dispatch = useDispatch();
 
   const handleChange = (e) => setInput(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(addIncome(input));
   };
 
   return (
     <div className="tabs">
       <div className="tabs__tab">
         <h2>Income</h2>
-        <form>
+        {handleSubmit && <h2>{income}</h2>}
+        <form onSubmit={handleSubmit} className="income__add-form">
           <input name="income" value={input} onChange={handleChange}></input>
           <button>Add</button>
         </form>
