5e8ee7319b650651e70806a20941c70af9722dd0
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-10-04 14:00:29 -0400

    Updated income slice to get and post to income values to the server

--- src/features/income/incomeSlice.js
+++ src/features/income/incomeSlice.js
@@ -1,22 +1,66 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "http://localhost:8080";
 
 const initialState = {
-  income: null,
+  income: 0,
+  isLoading: true,
 };
+
+export const getIncome = createAsyncThunk("income/getIncome", async () => {
+  try {
+    const res = await axios(url);
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const addIncome = createAsyncThunk(
+  "income/addIncome",
+  async (newIncome, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(url, newIncome);
+      return res.data;
+    } catch (error) {
+      if (!error.reponse) {
+        throw error;
+      }
+      return rejectWithValue(error.reponse.data);
+    }
+  }
+);
 
 export const incomeSlice = createSlice({
   name: "income",
   initialState,
-  reducers: {
-    addIncome: (state, action) => {
+  // reducers: {
+  //   addIncome: (state, action) => {
+  //     state.income = action.payload;
+  //   },
+  //   reduceIncome: (state, action) => {
+  //     state.income = action.payload;
+  //   },
+  // },
+  extraReducers: {
+    [getIncome.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getIncome.fulfilled]: (state, action) => {
+      state.isLoading = false;
       state.income = action.payload;
     },
-    reduceIncome: (state, action) => {
+    [getIncome.rejected]: (state) => {
+      state.isLoading = false;
+    },
+    [addIncome.fulfilled]: (state, action) => {
       state.income = action.payload;
     },
   },
 });
 
-export const { addIncome, reduceIncome } = incomeSlice.actions;
+// export const { addIncome, reduceIncome } = incomeSlice.actions;
 
 export default incomeSlice.reducer;
