fd12f636f5f72964e266c03eada6c42c804d2ecd
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-10-11 15:19:08 -0400

    Updated expenses endpoint to match updated api endpoint

--- src/features/expenses/expensesSlice.js
+++ src/features/expenses/expensesSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const url = "http://localhost:8080";
+const url = "http://localhost:8080/expenses";
 
 const initialState = {
   expenseItems: [],
