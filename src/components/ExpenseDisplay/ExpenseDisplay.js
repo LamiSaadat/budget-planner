1fa85c8d3c3ac3710894b23153ace9a36f223c76
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-10-12 18:27:07 -0400

    Removed unused component from expenses display. Updated styles to expenses display

--- src/components/ExpenseDisplay/ExpenseDisplay.js
+++ src/components/ExpenseDisplay/ExpenseDisplay.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import AddExpenseForm from "../AddExpenseForm/AddExpenseForm";
-import EditExpenseModal from "../EditExpenseModal/EditExpenseModal";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 
 import "./ExpenseDisplay.scss";
@@ -10,8 +9,8 @@ export default function ExpenseDisplay() {
   const { expenseItems } = useSelector((store) => store.expenses);
 
   return (
-    <div className="expensesContainer">
-      <div className="expensesContainer__items">
+    <div className="expenses-container">
+      <div className="expenses-container__items">
         {expenseItems.map((item, index) => {
           return <ExpenseItem key={index} {...item} />;
         })}
