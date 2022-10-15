7e47e57845de21ef49b1627710abfc78bc083a9c
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-10-15 16:47:46 -0400

    Updates svg attributes to comply with JSX rules

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -54,16 +54,16 @@ export default function Header() {
             <h2 className="tabs__amount">{income.income}</h2>
             <button onClick={handleEditBtn} className="tabs__edit-btn">
               <svg
-                class="w-6 h-6"
+                className="w-6 h-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </button>
